18b540431299a8b9fc7420aa62e68c2b22d50f06
tgck <taniguchiko@gmail.com>
2014-05-06 13:28:19 +0900

    モック一式を初回コミット。 以下確認済み: ・index.html からのブラウザ実行(Safari 5.1.9/Chrome) ・node-webkit 版アプリの実行(Mac OS X)

---
+++ src/_build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# node-webkitアプリケーションを作成します。
+# siteapp/src直下で実行してください
+zip serialLEDProgrammerWeb.nw * && mv serialLEDProgrammerWeb.nw ../build
+
+#### 以下 node-webkitのメモ ####
+# QuickStart
+# https://github.com/rogerwang/node-webkit#downloads
+
+# preBuildバイナリ
+# http://dl.node-webkit.org/v0.8.6/node-webkit-v0.8.6-osx-ia32.zip
+
+# サンプル
+# https://github.com/zcbenz/nw-sample-apps
+
+# 参考記事
+# http://shokai.org/blog/archives/8586
+####
