c230b2412df4ff83f5d8ef50a9fed9141ebc6e06
tgck <taniguchiko@gmail.com>
2014-05-18 23:15:25 +0900

    ・不具合修正

--- src/SrCmds.js
+++ src/SrCmds.js
@@ -473,7 +473,7 @@ function CmdSeesawFromStr(obj)
     val.push(parseInt(obj[i], 10));
   }
 
-  return new CmdBar(val[0], val[1], val[2], val[3]);
+  return new CmdSeesaw(val[0], val[1], val[2], val[3]);
 }
 
 // 名称の取得
