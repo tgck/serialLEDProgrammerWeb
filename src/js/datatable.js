79bc3fb027071454cf488d525e47a5de700fa029
carcon999 <carcon999@gmail.com>
2014-06-08 20:28:12 +0900

    作業状態保存処理を追加

--- src/js/datatable.js
+++ src/js/datatable.js
@@ -22,7 +22,46 @@ function initDataTable() {
 //			alert($(this).index());
 		}
 	});
-	
+
+	// 前回データのロード
+	loadDataTable("def_data");
+
 	// データ操作は以下を参照
 	// http://datatables.net/examples/api/
 }
+
+// 
+// 現在のデータテーブルを指定された名前でローカルストレージにjson形式で保存する。
+// 
+function saveDataTable(keyName) {
+	// localStorageが利用できる場合のみ保存する。
+	if (window.localStorage) {
+		// テーブルからコマンドオブジェクトを生成する
+		var oTable = $('#example').dataTable();
+		var x = oTable.fnGetData();
+		var json_text = JSON.stringify(x);
+		localStorage.setItem(keyName, json_text);
+	}
+}
+
+// 
+// 指定された名前のローカルストレージからデータを読み出してデータテーブルに格納
+// 
+function loadDataTable(keyName) {
+	// localStorageが利用できる場合のみロードする。
+	if (window.localStorage) {
+		var json_text = localStorage.getItem(keyName);
+		
+		if(json_text != null){
+			// テーブルからコマンドオブジェクトを生成する
+			var oTable = $('#example').dataTable();
+			// クリア
+			oTable.fnClearTable(false);
+			// json -> オブジェクト化
+			var obj = JSON.parse(json_text);
+			// テーブル追加
+			oTable.fnAddData(obj, true);
+		}
+	}
+}
+
